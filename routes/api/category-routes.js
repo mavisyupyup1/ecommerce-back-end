cfead9cb22bf33c38e0181d1e2f45a21f8e1399a
mavisyupyup1 <mavisyupyup1@github.com>
2021-12-28 10:12:53 -0500

    get all categories routes

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -1,11 +1,25 @@
 const router = require('express').Router();
-const { Category, Product } = require('../../models');
+const { Category, Product, ProductTag } = require('../../models');
 
 // The `/api/categories` endpoint
 
 router.get('/', (req, res) => {
   // find all categories
   // be sure to include its associated Products
+  Category.findAll({
+    attributes:['id','category_name'],
+    include:[
+      {
+        model:Product,
+        attributes:['id','product_name','price','stock','category_id']
+      }
+    ]
+  })
+  .then(dbCategoryData=>res.json(dbCategoryData))
+  .catch(err=>{
+    console.log(err);
+    res.status(500).json(err)
+  })
 });
 
 router.get('/:id', (req, res) => {
