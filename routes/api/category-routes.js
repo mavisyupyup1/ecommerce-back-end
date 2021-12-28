a8494be7b2d8bc15d65d79e531c1089c5cf065a4
mavisyupyup1 <mavisyupyup1@github.com>
2021-12-28 10:31:45 -0500

    delete and update categories routes

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -66,10 +66,39 @@ router.post('/', (req, res) => {
 
 router.put('/:id', (req, res) => {
   // update a category by its `id` value
+  Category.update(req.body,{
+   where:{id:req.params.id,
+   }
+  })
+  .then((dbCategoryData)=>{
+    if(!dbCategoryData){
+      res.status(404).json({message:'No category found with this Id.'})
+      return;
+    }
+    res.status(200).json(dbCategoryData)})
+  .catch((err)=>{
+    console.log(err);
+    res.status(400).json(err)
+  })
 });
+
 
 router.delete('/:id', (req, res) => {
   // delete a category by its `id` value
+  Category.destroy({
+    where:{id:req.params.id,
+    }
+   })
+   .then((dbCategoryData)=>{
+     if(!dbCategoryData){
+       res.status(404).json({message:'No category found with this Id.'})
+       return;
+     }
+     res.status(200).json(dbCategoryData)})
+   .catch((err)=>{
+     console.log(err);
+     res.status(500).json(err)
+   })
 });
 
 module.exports = router;
